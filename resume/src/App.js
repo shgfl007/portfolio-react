9af59386987b58594bbe2f398d417bed0dea18ba
shgfl007 <shgfl@hotmail.com>
2018-09-30 20:24:37 -0400

    push

---
+++ resume/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/HEADER/HEADER';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+
+      </div>
+    );
+  }
+}
+export default App;
